5bbd5464829f3350d80a68c6968466a2bebacc5e
mhshihab2604 <mhshihab2604@gmail.com>
2024-04-28 01:00:57 +0600

    update data and PUT Operation is done

--- index.js
+++ index.js
@@ -79,6 +79,16 @@ async function run() {
         const result = await cursor.toArray();
         res.send(result);
     })
+    app.get("/category/:id", async(req, res)=> {
+      const {id} = req.params
+      const category = id.split('&').join(' ')
+      
+        const cursor = paintingCollection.find({
+          subcategory_name:category
+          })
+        const result = await cursor.toArray();
+        res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
